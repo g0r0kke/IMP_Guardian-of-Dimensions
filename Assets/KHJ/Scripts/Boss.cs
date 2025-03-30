eb964354d31f4801da012f77e0710045c41b4cda
g0r0kke <jiniandjuni@gmail.com>
2025-03-30 09:39:14 +0900

    Feat: 플랫폼 안드로이드로 전환, UI 작업 중, Enemy 태그 추가

--- Assets/KHJ/Scripts/Boss.cs
+++ Assets/KHJ/Scripts/Boss.cs
@@ -83,7 +83,7 @@ public abstract class Boss : MonoBehaviour
         ChangeState<DeathState>();
     }
 
-    // 공통 메소드들
+    // 공통 메소드들 
     public virtual void TakeDamage(int damage)
     {
         health -= damage;
