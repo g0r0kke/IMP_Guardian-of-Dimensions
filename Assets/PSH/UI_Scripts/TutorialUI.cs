a143f7efff31f7c98cc048336aeaaa66f782f7ce
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2025-04-01 18:08:52 +0900

    [Feat] 이펙트 파일 추가, 시작화면 UI 기능 거의 완료
    이펙트 파일은 MasterStylize 라는 파일명으로 PSH - Effects 폴더 안에 있습니다

---
+++ Assets/PSH/UI_Scripts/TutorialUI.cs
@@ -0,0 +1,43 @@
+using Palmmedia.ReportGenerator.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUI : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public void SetControlMode(int ControlType)
+    {
+       // Settings.controlType = (EControlType)ControlType;
+    }
+
+    public void Close()
+    {
+        StartCoroutine(CloseAfterDelay());
+    }
+
+    private IEnumerator CloseAfterDelay()
+    {
+        animator.SetTrigger("close");
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+        animator.ResetTrigger("close");
+    }
+
+}
+
+
+
