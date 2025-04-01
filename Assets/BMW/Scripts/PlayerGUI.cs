0c40c979c03ed1576de2448fb7374409517a255a
alpha7179 <alpha_loading7179@naver.com>
2025-04-02 01:07:09 +0900

    Feat: Player GUI 스크립트 추가

---
+++ Assets/BMW/Scripts/PlayerGUI.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+public class PlayerGUI : MonoBehaviour
+{
+    public int playerHealth = 100;
+    public int playerHealthLimit = 100;
+    public int ultimateAttackGauge = 20;
+
+    public float basicAttackDelay = 1.0f;
+    public float ultimateAttackDelay = 1.0f;
+    public float avoidanceSkillDelay = 3.0f;
+    public float defenseSkillDelay = 6.0f;
+    public float healingSkillDelay = 10.0f;
+    public bool debug = true;
+
+    private BasicAttackController basicAttackController;
+    private UltimateAttackController ultimateAttackController;
+    private DefenseController defenseController;
+    private AvoidanceController avoidanceController;
+    private HealingController healingController;
+    private DamageController damageController;
+
+    private float basicAttackDelayTime;
+    private float ultimateAttackDelayTime;
+    private float avoidanceSkillDelayTime;
+    private float defenseSkillDelayTime;
+    private float healingSkillDelayTime;
+
+    private int previousHealth = 0;
+    private int previousGauge = 0;
+    public int currentGauge = 0;
+
+
+    void Start()
+    {
+        basicAttackController = GetComponent<BasicAttackController>();
+        ultimateAttackController = GetComponent<UltimateAttackController>();
+        defenseController = GetComponent<DefenseController>();
+        avoidanceController = GetComponent<AvoidanceController>();
+        healingController = GetComponent<HealingController>();
+        damageController = GetComponent<DamageController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playerHealth <= 0)
+        {
+            Gameover();
+        }
+        if (debug)
+        {
+            DEBUG();
+        }
+
+        basicAttackDelayTime = basicAttackController.delayTime;
+        ultimateAttackDelayTime = ultimateAttackController.delayTime;
+        defenseSkillDelayTime = defenseController.delayTime;
+        avoidanceSkillDelayTime = avoidanceController.delayTime;
+        healingSkillDelayTime = healingController.delayTime;
+    }
+
+    public void IncreaseGauge(int GaugeIncreaseAmount)
+    {
+        if(currentGauge < ultimateAttackGauge) currentGauge += GaugeIncreaseAmount;
+    }
+    public void IncreaseHealth(int HealingAmount)
+    {
+        
+        if (playerHealthLimit < playerHealth + HealingAmount) playerHealth = playerHealthLimit;
+        else playerHealth += HealingAmount;
+    }
+    public void decreaseHealth(int damageAmount)
+    {
+        playerHealth -= damageAmount;
+    }
+    void Gameover()
+    {
+        Debug.Log("Game Over");
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    public void DEBUG()
+    {
+        if (previousHealth != playerHealth)
+        {
+            Debug.Log("Player Health:" + playerHealth);
+            previousHealth = playerHealth;
+        }
+        if (previousGauge != currentGauge)
+        {
+            Debug.Log("currentGauge: " + currentGauge);
+            if (currentGauge >= ultimateAttackGauge) Debug.Log("Ultimate skill available");
+            previousGauge = currentGauge;
+        }
+        Debug.Log("basicAttackDelay:" + basicAttackDelayTime + "\n" +
+                  "ultimateAttackDelay:" + ultimateAttackDelayTime + "\n" +
+                  "avoidanceSkillDelay:" + avoidanceSkillDelayTime + "\n" +
+                  "defenseSkillDelay:" + defenseSkillDelayTime + "\n" +
+                  "healingSkillDelay:" + healingSkillDelayTime);
+    }
+}
