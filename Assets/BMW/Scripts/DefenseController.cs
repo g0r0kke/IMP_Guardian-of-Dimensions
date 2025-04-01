0bc3ffb2495f0593c57c244ce03b8c8627836f7b
alpha7179 <alpha_loading7179@naver.com>
2025-04-02 01:04:04 +0900

    Feat:Player방어모션추가

---
+++ Assets/BMW/Scripts/DefenseController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class DefenseController : MonoBehaviour
+{
+
+    public GameObject ShieldPrefab;
+    public Vector3 shieldScale = new Vector3(2f, 2f, 2f);
+    public float shieldDuration = 3.0f;
+    public float shieldDistance = 1.0f;
+
+    private GameObject shieldSphere;
+    public float delayTime = 0f;
+    private PlayerGUI playerGUI;
+
+    void Start()
+    {
+        playerGUI = GetComponent<PlayerGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool defensePressed = Input.GetKeyDown(KeyCode.C);
+
+        if (defensePressed && delayTime <= 0)
+        {
+            Defense();
+            delayTime = playerGUI.defenseSkillDelay;
+        }
+
+        if (delayTime > 0)
+        {
+            delayTime -= Time.deltaTime;
+            if (delayTime <= 0) delayTime = 0f;
+        }
+    }
+
+    void Defense()
+    {
+        if (shieldSphere != null)
+        {
+            Destroy(shieldSphere);
+        }
+        
+        //Vector3 shieldPosition = transform.position + transform.forward * shieldDistance;
+
+        shieldSphere = Instantiate(ShieldPrefab, transform.position, transform.rotation);
+
+        shieldSphere.transform.localScale = shieldScale;
+
+        shieldSphere.transform.parent = transform;
+
+        Destroy(shieldSphere, shieldDuration);
+    }
+}
