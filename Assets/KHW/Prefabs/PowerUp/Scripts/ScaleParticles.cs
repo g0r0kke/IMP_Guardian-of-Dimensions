f2cc2cb71d9563290c358964920dd6d2005e9306
12e0 <130565897+12e0@users.noreply.github.com>
2025-04-25 17:51:33 +0900

    Fix: 경고 제거. 접근 방식 변경

--- Assets/KHW/Prefabs/PowerUp/Scripts/ScaleParticles.cs
+++ Assets/KHW/Prefabs/PowerUp/Scripts/ScaleParticles.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class ScaleParticles : MonoBehaviour {
-	void Update () {
-		GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
-	}
+    void Update () {
+        var ps = GetComponent<ParticleSystem>();
+        var main = ps.main;
+        main.startSize = transform.lossyScale.magnitude;
+    }
 }
