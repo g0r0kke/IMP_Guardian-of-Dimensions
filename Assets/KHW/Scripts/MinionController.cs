915cef379d7b56b46566a4efd1efdea5e13b4d5b
12e0 <130565897+12e0@users.noreply.github.com>
2025-04-25 17:51:54 +0900

    Style: 주석 영어로 변경, 추가

--- Assets/KHW/Scripts/MinionController.cs
+++ Assets/KHW/Scripts/MinionController.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+/// Scripts created to implement Hobgoblin recall logic in proto type
 public class MinionController : MonoBehaviour
 {
     private Transform target;
@@ -13,7 +13,6 @@ public class MinionController : MonoBehaviour
         currentHP = maxHP;
     }
 
-    // 플레이어 타겟 지정
     public void SetTarget(Transform targetTransform)
     {
         target = targetTransform;
@@ -21,7 +20,6 @@ public class MinionController : MonoBehaviour
 
     void Update()
     {
-        // 간단하게 목표 위치로 이동 (NavMeshAgent가 아니라 직접 이동)
         if (target != null)
         {
             Vector3 direction = (target.position - transform.position).normalized;
@@ -31,22 +29,17 @@ public class MinionController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        // 플레이어에 부딪히면 데미지
         if (collision.gameObject.CompareTag("Player"))
         {
             SamplePlayer player = collision.gameObject.GetComponent<SamplePlayer>();
             if (player != null)
             {
                 player.TakeDamage((int)damage);
-                Debug.Log($"어둠의 소환물이 플레이어에게 {damage} 데미지를 입혔습니다!");
             }
         }
     }
 
-    public Transform GetTarget()
-    {
-        return target;
-    }
+    public Transform GetTarget() { return target; }
 
     public void TakeDamage(int dmg)
     {
